dce763e1a0ede7f0b7691ccdb1ab8af646ee8a4e
Pablo de los Santos <estudiodedalo@gmail.com>
2024-05-01 22:42:40 -0300

    Hola mundo. Variables

---
+++ 02-variables/variables.js
@@ -0,0 +1,20 @@
+let admin;
+let name = "Jonh";
+
+admin = name;
+
+alert(admin);
+
+// Dando el nombre correcto
+
+let ourPlanetName = "Tierra";
+
+let currentUserName = "Pablo";
+
+// ¿const mayúsculas?
+
+// Hardcodeada. El valor se conoce antes de la ejecución. Mayúscula
+const BIRTHDAY = "18.04.1982";
+
+// Evaluada durante la ejecución. Minúscula.
+const age = someCode(BIRTHDAY);
